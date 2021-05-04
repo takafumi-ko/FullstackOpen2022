fc3856222b0e77d67940956261a86c269704d3ff
古賀隆文 <t-koga@unext.jp>
2021-05-05 01:47:12 +0900

    Exercise 4.4

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs)=>{
+    if (blogs == null || blogs.length == null){
+        return 0
+    }
+
+    return blogs.reduce((sum,item)=>{
+       return sum + item.likes
+    },0)
+}
+
 module.exports = {
-    dummy
+    dummy,totalLikes
 }
