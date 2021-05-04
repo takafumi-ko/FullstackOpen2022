bea47446b1bf091d357884fc0192703ae08a3ac0
古賀隆文 <t-koga@unext.jp>
2021-05-05 03:15:04 +0900

    Exercise 4.6*

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -1,14 +1,27 @@
-
-const totalLikes = (blogs)=>{
-    if (blogs == null || blogs.length == null){
+const _ = require('lodash')
+const totalLikes = (blogs) => {
+    if (blogs == null || blogs.length == null) {
         return 0
     }
 
-    return blogs.reduce((sum,item)=>{
-       return sum + item.likes
-    },0)
+    return blogs.reduce((sum, item) => {
+        return sum + item.likes
+    }, 0)
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs == null || blogs.length == null) {
+        return 0
+    }
+    const counts = _.countBy(blogs, 'author')
+    const keys = Object.keys(counts)
+    const maxKey = _.maxBy(keys,(item)=>counts[item])
+    return {
+        author:maxKey,
+        blogs:counts[maxKey]
+    }
 }
 
 module.exports = {
-    totalLikes
+    totalLikes, mostBlogs
 }
