b06a2cafd9fb547186ce02641ec551de8de08fd8
古賀隆文 <t-koga@unext.jp>
2021-05-22 02:38:37 +0900

    Exercises 4.20

--- part4/backend/utils/middleware.js
+++ part4/backend/utils/middleware.js
@@ -16,16 +16,25 @@ const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({error: 'malformatted id'})
     } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7)
+    }
+    next()
+}
+
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
