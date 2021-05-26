8f8acfd9ce0bfb1deff3abcdb058e18396f0a1d4
古賀隆文 <t-koga@unext.jp>
2021-05-27 01:28:28 +0900

    Exercises 5.18

--- part4/bloglist-frontend-main/cypress/integration/blog.spec.js
+++ part4/bloglist-frontend-main/cypress/integration/blog.spec.js
@@ -1,6 +1,13 @@
 describe('Blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+        const user = {
+            name: 'Matti Luukkainen',
+            username: 'mluukkai',
+            password: 'salainen'
+        }
+        cy.request('POST', 'http://localhost:3003/api/users', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -8,5 +15,26 @@ describe('Blog app', function () {
         cy.get('#username')
         cy.get('#password')
         cy.contains('login')
+    })
+
+    describe('Login', function () {
+        it.only('succeeds with correct credentials', function () {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('salainen')
+            cy.contains('login').click()
+
+            cy.contains('Matti Luukkainen logged in')
+        })
+
+        it('fails with wrong credentials', function () {
+            cy.get('#username').type('mluukkai')
+            cy.get('#password').type('kyonen')
+            cy.contains('login').click()
+
+            cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+            cy.contains('wrong username or password')
+
+            cy.get('.caution').should('have.css', 'color', 'rgb(255, 0, 0)')
+        })
     })
 })
