a198c3f77688146dbe38049e2ff6a9f14aa5d680
古賀隆文 <t-koga@unext.jp>
2021-06-07 18:15:37 +0900

    Exercises 6.11

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { incrementVote } from "../reducers/anecdoteReducer";
+import { removeNotification, setNotification } from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
     const anecdotes = useSelector(state => state.anecdotes.sort((a, b) => {
@@ -8,9 +9,14 @@ const AnecdoteList = () => {
     }))
     const dispatch = useDispatch()
 
-    const vote = (id) => {
-        console.log('vote', id)
-        dispatch(incrementVote(id))
+    const vote = (anecdote) => {
+        console.log('vote', anecdote.id)
+        dispatch(incrementVote(anecdote.id))
+        const message = `you vote ${anecdote.content}`
+        dispatch(setNotification(message))
+        setTimeout(() => {
+            dispatch(removeNotification())
+        }, 5000)
     }
     return (
         anecdotes.map(anecdote =>
@@ -20,7 +26,7 @@ const AnecdoteList = () => {
                 </div>
                 <div>
                     has {anecdote.votes}
-                    <button onClick={() => vote(anecdote.id)}>vote</button>
+                    <button onClick={() => vote(anecdote)}>vote</button>
                 </div>
             </div>
         )
--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,7 +1,24 @@
 const notificationReducer = (state = '', action) => {
     switch (action.type) {
+        case 'SET':
+            return action.data.message
+        case 'REMOVE':
+            return ''
         default:
             return state
+    }
+}
+
+export const setNotification = (message) => {
+    return {
+        type: 'SET',
+        data: { message }
+    }
+}
+
+export const removeNotification = () => {
+    return {
+        type: "REMOVE"
     }
 }
 
