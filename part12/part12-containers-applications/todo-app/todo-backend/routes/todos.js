aa3bb074d3e40d6f299484629d784900b819fd06
takafumi-ko <extraextraction@gmail.com>
2022-08-13 17:47:09 +0900

    Exercises 12.19

--- part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
+++ part12/part12-containers-applications/todo-app/todo-backend/routes/todos.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Todo } = require('../mongo')
+const {Todo} = require('../mongo')
 const redis = require('../redis')
 const {log} = require("debug");
 const {ADDED_TODOS_KEY} = require('../util/config')
@@ -7,48 +7,51 @@ const router = express.Router();
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
-  res.send(todos);
+    const todos = await Todo.find({})
+    res.send(todos);
 });
 
 /* POST todo to listing. */
 router.post('/', async (req, res) => {
-  const todo = await Todo.create({
-    text: req.body.text,
-    done: false
-  })
+    const todo = await Todo.create({
+        text: req.body.text,
+        done: false
+    })
 
-  const numOfTodos = await redis.getAsync(ADDED_TODOS_KEY);
-  console.log("num",(Number(numOfTodos)+1).toString())
-  await redis.setAsync(ADDED_TODOS_KEY,(Number(numOfTodos)+1).toString());
-  res.send(todo);
+    const numOfTodos = await redis.getAsync(ADDED_TODOS_KEY);
+    await redis.setAsync(ADDED_TODOS_KEY, (Number(numOfTodos) + 1).toString());
+    res.send(todo);
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
+    const {id} = req.params
+    req.todo = await Todo.findById(id)
 
-  if (!req.todo) return res.sendStatus(404)
-
-  res.send(req.todo);
+    if (!req.todo) return res.sendStatus(404)
+    next()
 }
 
 /* DELETE todo. */
 singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
+    await req.todo.delete()
+    res.sendStatus(200);
 });
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+    res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+    const {modifiedCount} = await Todo.updateOne(req.todo, {done: true}, {runValidators: true})
+    if (modifiedCount === 1) {
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(204)
+    }
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
